289c90b75560e31d4bde51333f18bfedc58275ee
Айткожа Жанибек Ельдосович <@corp.itstep>
2019-01-08 11:57:37 +0600

    first commit

---
+++ Pract3/Money.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract3
+{
+    class Money
+    {
+        private int _par;
+        private int _count;
+
+        public int GetPar()
+        {
+            return _par;
+        }
+        public void SetPar(int par)
+        {
+            _par= par;
+        }
+
+
+        public int GetCount()
+        {
+            return _count;
+        }
+        public void SetCount(int count)
+        {
+            _count = count;
+        }
+
+
+
+        public Money(int par, int count)
+        {
+            _par = par;
+            _count = count;
+        }
+
+        public int GetSumm()
+        {
+            int summ = _par * _count;
+            return summ;
+        }
+
+        public string GetMoney()
+        {
+            string result = _par.ToString() + " " + _count.ToString(); 
+            return result;
+        }
+
+        public bool CanYouBuy(int N)
+        {
+            
+            if(_par*_count>N)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int HowMuchCanYouBuy(int n)
+        {
+            int countOfProuduct = (_par * _count) / n;
+            return countOfProuduct;
+        }
+    }
+}
---
+++ Pract3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money(20, 50);
+            Console.WriteLine(money.GetMoney());
+            Console.WriteLine(money.CanYouBuy(500));
+            Console.WriteLine(money.HowMuchCanYouBuy(50));
+            money.SetCount(100);
+            Console.WriteLine(money.GetSumm());
+
+            Console.ReadLine();
+        }
+    }
+
+}
